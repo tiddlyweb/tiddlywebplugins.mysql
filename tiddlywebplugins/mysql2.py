7bf04a2f9cd2019dc828ae365d63009b1015de40
Chris Dent <chris.dent@gmail.com>
2011-05-12 20:03:52 +0100

    releae 2.1.5

--- tiddlywebplugins/mysql2.py
+++ tiddlywebplugins/mysql2.py
@@ -41,7 +41,7 @@ import logging
 #logging.getLogger('sqlalchemy.orm.unitofwork').setLevel(logging.DEBUG)
 #logging.getLogger('sqlalchemy.pool').setLevel(logging.DEBUG)
 
-__version__ = '2.1.4'
+__version__ = '2.1.5'
 
 ENGINE = None
 MAPPED = False
