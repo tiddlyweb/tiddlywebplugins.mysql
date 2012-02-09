1d9401e3ccbdeaa75528553ab06a432fdd4e39fd
Chris Dent <chris.dent@gmail.com>
2012-02-09 15:03:54 +0000

    release 2.1.13

--- tiddlywebplugins/mysql2.py
+++ tiddlywebplugins/mysql2.py
@@ -42,7 +42,7 @@ import logging
 #logging.getLogger('sqlalchemy.orm.unitofwork').setLevel(logging.DEBUG)
 #logging.getLogger('sqlalchemy.pool').setLevel(logging.DEBUG)
 
-__version__ = '2.1.12'
+__version__ = '2.1.13'
 
 ENGINE = None
 MAPPED = False
