457c53531c7efaeaf32703f2309e0dd7c0371de8
Chris Dent <chris.dent@gmail.com>
2012-07-18 21:43:01 +0100

    release 3.0.5
    * refactoring into multiple modules.

--- tiddlywebplugins/mysql3/__init__.py
+++ tiddlywebplugins/mysql3/__init__.py
@@ -39,7 +39,7 @@ import logging
 #logging.getLogger('sqlalchemy.orm.unitofwork').setLevel(logging.DEBUG)
 #logging.getLogger('sqlalchemy.pool').setLevel(logging.DEBUG)
 
-__version__ = '3.0.4'
+__version__ = '3.0.5'
 
 ENGINE = None
 MAPPED = False
