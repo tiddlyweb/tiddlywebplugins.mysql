290e275aaa8f354136f774b43a981752422960ae
Chris Dent <chris.dent@gmail.com>
2013-06-25 12:35:45 +0100

    release 3.0.12

--- tiddlywebplugins/mysql3/__init__.py
+++ tiddlywebplugins/mysql3/__init__.py
@@ -39,7 +39,7 @@ import logging
 #logging.getLogger('sqlalchemy.orm.unitofwork').setLevel(logging.DEBUG)
 #logging.getLogger('sqlalchemy.pool').setLevel(logging.DEBUG)
 
-__version__ = '3.0.11'
+__version__ = '3.0.12'
 
 ENGINE = None
 MAPPED = False
