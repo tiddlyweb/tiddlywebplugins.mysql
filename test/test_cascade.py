2059f071f659b4d8bbe697918a83ba75c5c7b397
Chris Dent <chris.dent@gmail.com>
2013-08-13 17:38:56 +0100

    Correct import of sTiddler
    After changes to mysql3/__init__.py it is now in sqlalchemy3

--- test/test_cascade.py
+++ test/test_cascade.py
@@ -9,8 +9,8 @@ from tiddlyweb.store import Store
 from tiddlyweb.model.bag import Bag
 from tiddlyweb.model.tiddler import Tiddler
 
-from tiddlywebplugins.mysql3 import (Base, sTiddler, Session)
-from tiddlywebplugins.sqlalchemy3 import (sText, sTag, sRevision, sField)
+from tiddlywebplugins.mysql3 import (Base, Session)
+from tiddlywebplugins.sqlalchemy3 import (sText, sTiddler, sTag, sRevision, sField)
 
 
 def setup_module(module):
